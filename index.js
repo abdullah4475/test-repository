f404b61742e867b6584f03c2a756d5cff6a73f1a
Muhammad <m.abdullah4475@gmail.com>
2023-02-06 17:33:57 -0800

    restructured

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 
 console.log('hello world')
 console.log('new-branch')
+console.log('restructured')
 const port = 3000
 app.listen(port, () => {
     console.log(`Server listening on Port ${port}`);
